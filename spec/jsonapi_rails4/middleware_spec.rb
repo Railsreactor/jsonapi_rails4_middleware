e05b876b440f97cc592c44393a784e142bf4c54f
Denis Sergienko <olol.toor@gmail.com>
2015-11-18 21:48:20 +0200

    spec added

--- spec/jsonapi_rails4/middleware_spec.rb
+++ spec/jsonapi_rails4/middleware_spec.rb
@@ -1,5 +1,126 @@
 describe JsonapiRails4::Middleware do
-  it 'test' do
-    expect(described_class).to_not be_nil
+  let(:app) { ->(input) { [200, input, 'app'] } }
+  let(:response_body) { subject.call(env)[1]['rack.input'].read }
+
+  subject { described_class.new(app) }
+
+  context 'when content type is json' do
+    context 'when json follows jsonapi spec' do
+        context 'POST request' do
+          let(:env) do
+            Rack::MockRequest.env_for('', {
+              'rack.input' => StringIO.new({
+                data: {
+                  type: 'users',
+                  attributes: {
+                    email: 'example.com',
+                    password: '12345678'
+                  }
+                }
+              }.to_json),
+              'CONTENT_TYPE' => 'application/json',
+              'REQUEST_METHOD' => 'POST'
+            })
+          end
+
+          it 'changes request body' do
+            expect(JSON.parse(response_body)).to eq({
+              'user' => {
+                'email' => 'example.com',
+                'password' => '12345678'
+              }
+            })
+          end
+        end
+
+        context 'PATCH request' do
+          let(:env) do
+            Rack::MockRequest.env_for('', {
+              'rack.input' => StringIO.new({
+                data: {
+                  type: 'users',
+                  id: 5,
+                  attributes: {
+                    email: 'example.com',
+                    password: '12345678'
+                  }
+                }
+              }.to_json),
+              'CONTENT_TYPE' => 'application/json',
+              'REQUEST_METHOD' => 'PATCH'
+            })
+          end
+
+          it 'changes request body' do
+            expect(JSON.parse(response_body)).to eq(
+              'user' => {
+                'id' => 5,
+                'email' => 'example.com',
+                'password' => '12345678'
+              }
+            )
+          end
+        end
+    end
+
+    context 'when json does not follow jsonapi spec' do
+      let(:env) do
+        Rack::MockRequest.env_for('', {
+          'rack.input' => StringIO.new({
+            data: {
+              model: 'users',
+              params: {
+                email: 'example.com',
+                password: '12345678'
+              }
+            }
+          }.to_json),
+          'CONTENT_TYPE' => 'application/json',
+          'REQUEST_METHOD' => 'POST'
+        })
+      end
+
+      it 'does not change request body' do
+        expect(JSON.parse(response_body)).to eq(
+          'data' => {
+            'model' => 'users',
+            'params' => {
+              'email' => 'example.com',
+              'password' => '12345678'
+            }
+          }
+        )
+      end
+    end
+  end
+
+  context 'content type is not json' do
+    let(:env) do
+      Rack::MockRequest.env_for('', {
+        'rack.input' => StringIO.new({
+          data: {
+            type: 'users',
+            attributes: {
+              email: 'example.com',
+              password: '12345678'
+            }
+          }
+        }.to_json),
+        'CONTENT_TYPE' => 'text/html',
+        'REQUEST_METHOD' => 'POST'
+      })
+    end
+
+    it 'does not change request body' do
+      expect(JSON.parse(response_body)).to eq(
+        'data' => {
+          'type' => 'users',
+          'attributes' => {
+            'email' => 'example.com',
+            'password' => '12345678'
+          }
+        }
+      )
+    end
   end
 end
