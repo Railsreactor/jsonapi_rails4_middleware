e344740b8b1d4b3d427e2862b7ffe3cff9515afc
Denis <denis.yakovenko@railsreactor.com>
2015-11-20 15:33:54 +0200

    added empty body test

--- spec/jsonapi_rails4/middleware_spec.rb
+++ spec/jsonapi_rails4/middleware_spec.rb
@@ -6,78 +6,70 @@ describe JsonapiRails4::Middleware do
 
   context 'when content type is json' do
     context 'when json follows jsonapi spec' do
-        context 'POST request' do
-          let(:env) do
-            Rack::MockRequest.env_for('', {
-              'rack.input' => StringIO.new({
-                data: {
-                  type: 'users',
-                  attributes: {
-                    email: 'example.com',
-                    password: '12345678'
-                  }
-                }
-              }.to_json),
-              'CONTENT_TYPE' => 'application/json',
-              'REQUEST_METHOD' => 'POST'
-            })
-          end
-
-          it 'changes request body' do
-            expect(JSON.parse(response_body)).to eq({
-              'user' => {
-                'email' => 'example.com',
-                'password' => '12345678'
-              }
-            })
-          end
-        end
-
-        context 'PATCH request' do
-          let(:env) do
-            Rack::MockRequest.env_for('', {
-              'rack.input' => StringIO.new({
-                data: {
-                  type: 'users',
-                  id: 5,
-                  attributes: {
-                    email: 'example.com',
-                    password: '12345678'
-                  }
-                }
-              }.to_json),
-              'CONTENT_TYPE' => 'application/json',
-              'REQUEST_METHOD' => 'PATCH'
-            })
-          end
-
-          it 'changes request body' do
-            expect(JSON.parse(response_body)).to eq(
-              'user' => {
-                'id' => 5,
-                'email' => 'example.com',
-                'password' => '12345678'
-              }
-            )
-          end
-        end
-    end
-
-    context 'when json does not follow jsonapi spec' do
-      let(:env) do
-        Rack::MockRequest.env_for('', {
-          'rack.input' => StringIO.new({
+      context 'POST request' do
+        let(:env) do
+          Rack::MockRequest.env_for('', 'rack.input' => StringIO.new({
             data: {
-              model: 'users',
-              params: {
-                email: 'example.com',
+              type: 'users',
+              attributes: {
+                email: 'john.doe@example.com',
                 password: '12345678'
               }
             }
           }.to_json),
+            'CONTENT_TYPE' => 'application/json',
+            'REQUEST_METHOD' => 'POST')
+        end
+
+        it 'changes request body' do
+          expect(JSON.parse(response_body)).to eq('user' => {
+                                                    'email' => 'john.doe@example.com',
+                                                    'password' => '12345678'
+                                                  })
+        end
+      end
+
+      context 'PATCH request' do
+        let(:env) do
+          Rack::MockRequest.env_for('', 'rack.input' => StringIO.new({
+            data: {
+              type: 'users',
+              id: 5,
+              attributes: {
+                email: 'john.doe@example.com',
+                password: '12345678'
+              }
+            }
+          }.to_json),
+            'CONTENT_TYPE' => 'application/json',
+            'REQUEST_METHOD' => 'PATCH')
+        end
+
+        it 'changes request body' do
+          expect(JSON.parse(response_body)).to eq(
+            'user' => {
+              'id' => 5,
+              'email' => 'john.doe@example.com',
+              'password' => '12345678'
+            }
+          )
+        end
+      end
+    end
+
+    context 'when json does not follow jsonapi spec' do
+      let(:env) do
+        Rack::MockRequest.env_for('', 'rack.input' => StringIO.new({
+          data: {
+            model: 'users',
+            params: {
+              email: 'john.doe@example.com',
+              password: '12345678'
+            }
+          }
+        }.to_json),
           'CONTENT_TYPE' => 'application/json',
-          'REQUEST_METHOD' => 'POST'
-        })
+          'REQUEST_METHOD' => 'POST')
       end
 
       it 'does not change request body' do
@@ -85,7 +77,7 @@ describe JsonapiRails4::Middleware do
           'data' => {
             'model' => 'users',
             'params' => {
-              'email' => 'example.com',
+              'email' => 'john.doe@example.com',
               'password' => '12345678'
             }
           }
@@ -96,19 +88,17 @@ describe JsonapiRails4::Middleware do
 
   context 'content type is not json' do
     let(:env) do
-      Rack::MockRequest.env_for('', {
-        'rack.input' => StringIO.new({
-          data: {
-            type: 'users',
-            attributes: {
-              email: 'example.com',
-              password: '12345678'
-            }
+      Rack::MockRequest.env_for('', 'rack.input' => StringIO.new({
+        data: {
+          type: 'users',
+          attributes: {
+            email: 'john.doe@example.com',
+            password: '12345678'
           }
-        }.to_json),
+        }
+      }.to_json),
         'CONTENT_TYPE' => 'text/html',
-        'REQUEST_METHOD' => 'POST'
-      })
+        'REQUEST_METHOD' => 'POST')
     end
 
     it 'does not change request body' do
@@ -116,11 +106,24 @@ describe JsonapiRails4::Middleware do
         'data' => {
           'type' => 'users',
           'attributes' => {
-            'email' => 'example.com',
+            'email' => 'john.doe@example.com',
             'password' => '12345678'
           }
         }
       )
+    end
+  end
+
+  context 'content body is empty' do
+    let(:env) do
+      Rack::MockRequest.env_for('',
+        'rack.input' => StringIO.new({}.to_json),
+        'CONTENT_TYPE' => 'text/html',
+        'REQUEST_METHOD' => 'POST')
+    end
+
+    it 'does not change request body' do
+      expect(JSON.parse(response_body)).to eq({})
     end
   end
 end
